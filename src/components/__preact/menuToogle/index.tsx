16575110bb573e6c54c723a40b28a91c5f0a53a0
Nathan Molina <itsjustnatemolina@gmail.com>
2025-02-24 09:04:14 -0300

    components created + pages setted + pages content WIP

--- src/components/__preact/menuToogle/index.tsx
+++ src/components/__preact/menuToogle/index.tsx
@@ -4,13 +4,17 @@ import style from "./style.module.scss";
 const MenuToogle = () => {
     const [ menuStatus, setMenuStatus ] = useState<boolean>(false);
 
+    const toogleMenu = () => {
+        return setMenuStatus(!menuStatus);
+    };
+
     return (
         <>
             <button
                 id="menu"
                 aria-label="Hamburguer menu icon"
                 class={`${style.mobileHeader} ${menuStatus ? style.menuOpened : ""}`} 
-                onClick={() => setMenuStatus(!menuStatus)}
+                onClick={toogleMenu}
             >
                 {!menuStatus ? 
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.5 10h15m-15-5h15m-15 10h15"/></svg> 
